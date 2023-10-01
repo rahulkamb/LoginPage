639c5e48c9305ab5db122a315c355b883315ceb4
rahulkamb <rahulkamble12373@gmail.com>
2023-10-01 14:06:38 +0530

    backend api in dot net

---
+++ backendloginpage/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using backendloginpage.Data;
+using backendloginpage.DTO;
+using backendloginpage.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backendloginpage.Controllers
+{
+    [Route("api/Login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly DBContextLayer dBContextLayer;
+
+        public LoginController( DBContextLayer _dBContextLayer)
+        {
+            dBContextLayer = _dBContextLayer;
+        }
+
+
+        [HttpPost]
+        public IActionResult Registration([FromBody] AddUsersdto addUsersdto)
+        {
+            var registration = new LoginDetails
+            {
+                Name = addUsersdto.Name,
+                EmailId = addUsersdto.EmailId,
+                Password = addUsersdto.Password,
+                IsActive = addUsersdto.IsActive
+            };
+
+            dBContextLayer.M_User.Add(registration);
+            dBContextLayer.SaveChanges();
+
+            return Ok(registration);
+        }   
+    }
+}
---
+++ backendloginpage/Data/DBContextLayer.cs
@@ -0,0 +1,16 @@
+﻿
+using backendloginpage.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendloginpage.Data
+{
+    public class DBContextLayer : DbContext
+    {
+        public DBContextLayer(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<LoginDetails> M_User { get; set; }
+    }
+}
